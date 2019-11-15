a169d54e09209bf5c9ab37bd7a247339939b7a9b
MDey <michaeldey@hotmail.com>
2019-11-14 22:23:09 -0700

    adding shebang_test.py

---
+++ python_tools/shebang_test.py
@@ -0,0 +1,11 @@
+#! /usr/bin/python3
+
+print("There is a shebang at the top of this file.")
+print("The address at the top indicates where python3 is located")
+print("The address is only valid on a linux machine")
+print("Linux: #! /usr/bin/python3")
+print("Mac: #! /usr/bin/env/python3")
+print("Windows: #! python3")
+
+
+print("The permissions of this file are chmod 777")
